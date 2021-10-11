fcf02c975cca5304a4cbb818e522b3991ccea092
arjun-kathail <arjunkathail5@gmail.com>
2021-10-12 05:29:56 +0530

    App.js corrected

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import SignUp from "./components/signup/index"
+// import SignUp from "./components/signup/index"
 
 const App = () => {
-    return <SignUp />;
+    return <h1>First Light</h1>;
 };
 
 export default App;
