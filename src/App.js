b68ea6555c7fdf0faa9324245c1d504578685eb8
Utkarsh Goel <ugoel911@gmail.com>
2021-10-13 17:29:30 +0530

    top nav added

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@ import React, { useContext } from "react";
 import { Switch, Route, Redirect } from "react-router";
 import userContext from "./context/userContext";
 import SignUp from "./components/signup";
+import TopNav from "./components/TopNav";
 
 const App = () => {
     var routes = null;
     const [user, setUser] = useContext(userContext);
     if (user == null) {
         routes = (
-            <Switch>
-                <Route path="/login" component={SignUp} />
-                <Redirect to="/"></Redirect>
-            </Switch>
+            <div>
+                <Switch>
+                    <Route path="/login" component={SignUp} />
+                    <Redirect to="/"></Redirect>
+                </Switch>
+            </div>
         );
     } else {
         routes = (
             <div>
+                <TopNav />
                 <Switch>
                     <Redirect to="/news"></Redirect>
                 </Switch>
