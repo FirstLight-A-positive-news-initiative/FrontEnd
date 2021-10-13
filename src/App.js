d525d364b1e47293082cb11131f6cc27823057a4
Utkarsh Goel <ugoel911@gmail.com>
2021-10-13 13:13:35 +0530

    context and router added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Switch, Route, Redirect } from "react-router";
+import userContext from "./context/userContext";
 
 const App = () => {
-    return <h1>Project - FirstLight</h1>;
+    var routes = null;
+    const [user, setUser] = useContext(userContext);
+    if (user == null) {
+        routes = (
+            <Switch>
+                <Redirect></Redirect>
+            </Switch>
+        );
+    } else {
+        routes = (
+            <div>
+                <Switch>
+                    <Redirect></Redirect>
+                </Switch>
+            </div>
+        );
+    }
+    return <div className="App">{routes}</div>;
 };
 
 export default App;
---
+++ src/context/userContext.js
@@ -0,0 +1,15 @@
+import React, { useState, createContext } from "react";
+
+export const userContext = createContext();
+
+export const UserProvider = (props) => {
+    const [user, setUser] = useState(null);
+
+    return (
+        <userContext.Provider value={[user, setUser]}>
+            {props.children}
+        </userContext.Provider>
+    );
+};
+
+export default userContext;
