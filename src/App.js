4cb7d4f488c4ae4232c10ee4d8ca28c5d4258088
arjun-kathail <arjunkathail5@gmail.com>
2021-10-14 22:03:19 +0530

    Google login and css corrected

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 // import SignUp from "./components/signup/index"
 
 const App = () => {
-    return <h1>First Light</h1>;
+    return <h1>FirstLight</h1>;
 };
 
 export default App;
